49cac60c6c8520abe0b5e72cd18faa34e0a8df17
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2023-04-20 04:05:44 +0700

    Add files via upload

---
+++ so sánh 2 mảng.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdio.h>
+int main() {
+    char str1[] = "Hello";
+    char str2[] = "World";
+    int sosanh = strcmp(str1, str2);
+    // sosanh = 0 nếu 2 mảng giống nhau
+    // >0 nếu mảng 1 lớn hơn mảng 2 và ngược lại
+    return 0;
+}
