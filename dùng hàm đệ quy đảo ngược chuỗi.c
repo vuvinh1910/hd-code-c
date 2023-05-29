45719148f0bcc9298331e681bcc8b8bd52c73e89
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2023-05-29 21:31:26 +0700

    Add files via upload

---
+++ dùng hàm đệ quy đảo ngược chuỗi.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+
+void Reverse(){
+    char c;
+    scanf("%c", &c);
+    if(c != '\n'){
+        Reverse();
+        printf("%c", c);
+    }
+}
+ 
+int main(){
+    printf("Enter you sentence: ");
+    Reverse();
+}
