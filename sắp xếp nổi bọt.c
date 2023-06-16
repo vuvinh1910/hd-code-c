618ddc3fdb1190347755b9cf8d29b39138d95e86
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2023-06-16 22:44:52 +0700

    Update sắp xếp nổi bọt.c

--- sắp xếp nổi bọt.c
+++ sắp xếp nổi bọt.c
@@ -1,21 +1,33 @@
 #include <stdio.h>
-void nhap(char a[]){
-	gets(a);
+
+void xuat(int b[],int n){
+    for(int i=0;i<n;i++){
+        printf("%d ",b[i]);
+    }
+    printf("\n");
 }
+
 int main(){
-	char a[100];
-	nhap(a);
-	int t=1;
+	int a;scanf("%d",&a);
+    int b[100];
+    for(int i=0;i<a;i++){
+        scanf("%d",&b[i]);
+    }
+	int t=1;int y=0;
 	while(t!=0){
 		t=0;
-		for(int i=0;i<strlen(a)-1;i++){
-			if(a[i]>a[i+1]){
-				int k=a[i];
-				a[i]=a[i+1];
-				a[i+1]=k;
+		for(int i=0;i<a-1;i++){
+			if(b[i]>b[i+1]){
+				int k=b[i];
+				b[i]=b[i+1];
+				b[i+1]=k;
 				t=1;
 			}
 		}
+        if(t==1){
+            y++;
+        printf("Buoc %d: ",y);
+        xuat(b,a);
+        }
 	}
-	printf ("%s",a);
 }
