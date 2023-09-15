69e12d1ee2add5a68969a2d8e1f17d06ca305caa
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2023-09-15 19:04:33 +0700

    Update UCLN-BCNN.c

--- UCLN-BCNN.c
+++ UCLN-BCNN.c
@@ -1,19 +1,12 @@
 #include <stdio.h>
 #include <math.h>
 
-int main() {
-    int a,b;
-    scanf("%d %d",&a,&b);
-    int c=a*b;
-    if (a == 0 || b == 0){
-        a=a+b;
-    } else {
-    while(a!=b){
-        if (a>b) {a=a-b;}
-        else {b=b-a;}
-    }
-    }
-    printf("%d\n",a);
-    printf("%d\n",c/a);
-    return 0;
+int USCLN(int a, int b) {
+    if (b == 0) return a;
+    return USCLN(b, a % b);
+}
+ 
+
+int BSCNN(int a, int b) {
+    return (a * b) / USCLN(a, b);
 }
